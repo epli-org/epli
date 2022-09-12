1d61602f534cd4457403181dc410d0a3db50c21d
jomanw <jordmanwick@gmail.com>
2022-09-11 17:51:26 -0700

    Added caching for backend

--- server/app.py
+++ server/app.py
@@ -8,8 +8,16 @@ import openai
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_papers_cached():
+    papers = []
+    for index, paper_tag in enumerate(os.listdir("server/papers")):
+        print(paper_tag)
+        papers.append(paper_tag)
+        if index >= 10:
+            break
+    paper_string = ', \n'.join(papers)
+    
+    return f"<p>Hello, World! Papers in our 'database': {paper_string}.</p>"
 
 
 def download_file(download_url, download_folder, download_filename):
@@ -40,6 +48,40 @@ def get_arxiv_abstract(txt_file_path):
 
 def get_explanation_prompt(abstract):
     return f"Explain '{abstract}' like I am a fifth grader, in 3 sentences or less."
+
+def llm_paper_summary(arxiv_id, abstract):
+    """Returns the LLM-Generated summary of the paper, either fresh or cached
+    """
+    # Set the API key
+    openai.api_key = "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"
+    summary_filename = f"{arxiv_id}.txt"
+    summary_directory = "server/summaries"
+    if not os.path.exists(summary_directory):
+        os.mkdir(summary_directory)
+    # Check if it's in the cache
+    if (summary_filename in os.listdir(summary_directory)):
+        # It's in the cache, lets just return the cached version
+        with open(os.path.join(summary_directory, summary_filename), 'r') as f:
+            paper_summary = f.readlines()[0]
+            print("From Cache")
+            print(paper_summary)
+            return paper_summary
+    
+    # If it's not in the cache, summarize via the OpenAI API and cache the result
+    else:
+        response = openai.Completion.create(
+            model="text-davinci-002", 
+            prompt=get_explanation_prompt(abstract), 
+            temperature=1,
+            max_tokens=100
+        )
+        summary = response['choices'][0]['text'].strip()
+        print("Abstract: ", abstract)
+        with open(os.path.join(summary_directory, summary_filename), 'w') as f:
+            f.write(summary)
+
+        return summary
+
 
 @app.route("/main_paper_information")
 def main_paper_info():
@@ -73,7 +115,9 @@ def main_paper_info():
     download_url = f"https://arxiv.org/pdf/{arxiv_id}.pdf"
 
     # Grab the PDF and download it (TODO: check cache in the future)
-    download_directory = "papers"
+    download_directory = "server/papers"
+    if not os.path.exists(download_directory):
+        os.mkdir(download_directory)
     download_file(download_url, download_directory, arxiv_id)
 
     # Extract the text from the PDF
@@ -92,15 +136,16 @@ def main_paper_info():
     abstract = get_arxiv_abstract(txt_file_path)
 
     # Use GPT-3 to summarize the PDF
-    openai.api_key = "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"
-    response = openai.Completion.create(
-        model="text-davinci-002", 
-        prompt=get_explanation_prompt(abstract), 
-        temperature=1,
-        max_tokens=10
-    )
-    summary = response['choices'][0]['text']
-    print("Abstract: ", abstract)
+    summary = llm_paper_summary(arxiv_id, abstract)
+    # openai.api_key = "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"
+    # response = openai.Completion.create(
+    #     model="text-davinci-002", 
+    #     prompt=get_explanation_prompt(abstract), 
+    #     temperature=1,
+    #     max_tokens=10
+    # )
+    # summary = response['choices'][0]['text']
+    # print("Abstract: ", abstract)
 
     # Return the abstract and summary in a JSON response object
     test_response = {
